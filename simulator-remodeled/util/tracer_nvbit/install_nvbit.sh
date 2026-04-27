2fac7e46757859e65e87db1cec83f051faa853ca
Jihyun Ryoo <jihyun.ryoo@bytedance.com>
2026-04-27 20:44:15 +0000

    Enable for aarch for Blackwell

--- simulator-remodeled/util/tracer_nvbit/install_nvbit.sh
+++ simulator-remodeled/util/tracer_nvbit/install_nvbit.sh
@@ -1,9 +1,18 @@
 export BASH_ROOT="$( cd "$( dirname "$BASH_SOURCE" )" && pwd )"
 
+ARCH=$(uname -m)
+if [ "$ARCH" = "aarch64" ]; then
+    NVBIT_FILE="nvbit-Linux-aarch64-1.7.5.tar.bz2"
+    NVBIT_DIR="nvbit_release_aarch64"
+else
+    NVBIT_FILE="nvbit-Linux-x86_64-1.7.5.tar.bz2"
+    NVBIT_DIR="nvbit_release_x86_64"
+fi
+
 rm -rf $BASH_ROOT/nvbit_release
-wget https://github.com/NVlabs/NVBit/releases/download/v1.7.5/nvbit-Linux-x86_64-1.7.5.tar.bz2
-tar -xf nvbit-Linux-x86_64-1.7.5.tar.bz2 -C $BASH_ROOT
-rm nvbit-Linux-x86_64-1.7.5.tar.bz2
-mv $BASH_ROOT/nvbit_release_x86_64 $BASH_ROOT/nvbit_release
+wget https://github.com/NVlabs/NVBit/releases/download/v1.7.5/$NVBIT_FILE
+tar -xf $NVBIT_FILE -C $BASH_ROOT
+rm $NVBIT_FILE
+mv $BASH_ROOT/$NVBIT_DIR $BASH_ROOT/nvbit_release
 
 
